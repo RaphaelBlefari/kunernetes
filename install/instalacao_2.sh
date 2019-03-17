d66a3d95a4fd926be31e789e91f722f3e4c992e8
Raphael Blefari <blefari_101@hotmail.com>
2019-03-16 22:09:54 -0300

    Adicionado install

--- install/instalacao_2.sh
+++ install/instalacao_2.sh
@@ -23,15 +23,5 @@ echo "Disabilitando Firewall"
 sudo systemctl disable firewalld
 sudo systemctl stop firewalld
 
-ip="$(ip a s|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}'| grep -m "1" .)"
-
-echo "Iniciando Cluster"
-kubeadm init --apiserver-advertise-address="$(echo $ip)" --pod-network-cidr=10.244.0.0/16
-
-echo "Adicionar configs"
-mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-echo "deploy Flannel"
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
-
+echo "Sucesso"
+"Continuar para instalação 3"
